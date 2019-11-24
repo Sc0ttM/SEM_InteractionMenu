3bbbcdc65ee938ffe30014fe75aaf3e87d1d269b
Scott M <46624922+Sc0ttM@users.noreply.github.com>
2019-11-24 11:41:37 +0000

    Added Version Checker

--- SEM_InteractionMenu/server.lua
+++ SEM_InteractionMenu/server.lua
@@ -44,3 +44,40 @@ RegisterServerEvent('SEM_Ads')
 AddEventHandler('SEM_Ads', function(Text, Name, Loc, File)
     TriggerClientEvent('SEM_SyncAds', -1, Text, Name, Loc, File)
 end)
+
+
+
+Citizen.CreateThread(function()
+	local CurrentVersion = json.decode(LoadResourceFile(GetCurrentResourceName(), 'version.json')).version
+
+	function VersionCheckHTTPRequest()
+		PerformHttpRequest('https://semdevelopment.com/releases/interactionmenu/info/version.json', VersionCheck, 'GET')
+	end
+
+	function VersionCheck(err, response, headers)
+		if err == 200 then
+			local Data = json.decode(response)
+			if tonumber(CurrentVersion) < tonumber(Data.NewestVersion) then
+				print('\n--------------------------------------------------------------------------')
+				print('\nSEM_InteractionMenu is outdated!')
+				print('Current Version: ^2' .. Data.NewestVersion .. '^7')
+				print('Your Version: ^1' .. CurrentVersion .. '^7')
+				print('Please download the lastest version from ^5' .. Data.DownloadLocation .. '^7')
+				if Data.Changes ~= '' then
+					print('\nChanges: ' .. Data.Changes)
+				end
+				print('\n--------------------------------------------------------------------------\n')
+			elseif tonumber(CurrentVersion) > tonumber(Data.NewestVersion) then
+				print('^3Your version of SEM_InteractionMenu is higher than the current version!^7')
+			else
+				print('^2SEM_InteractionMenu is up to date!')
+			end
+		else
+			print('^1SEM_InteractionMenu Version Check Failed!^7')
+		end
+		
+		SetTimeout(60000000, VersionCheckHTTPRequest)
+	end
+
+	VersionCheckHTTPRequest()
+end)
